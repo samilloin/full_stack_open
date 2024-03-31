d7f9bdd38296879fd30321fc97a875ac1b72c458
Sami Saariaho <sami.saariaho@op.fi>
2024-03-31 12:21:14 +0300

    1.6: unicafe step1

---
+++ part_1/osa1/unicafe/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Please Give Feedback</h1>
+      <br/><br/>
+      <Button text='good' handleClick={ () => setGood(good + 1) } />&nbsp;
+      <Button text='neutral' handleClick={ () => setNeutral(neutral + 1) } />&nbsp;
+      <Button text='bad' handleClick={ () => setBad(bad + 1)} />&nbsp;
+      <br/><br/>
+      <h2>Statistics</h2>
+      <br/>
+      good {good}<br/>
+      neutral {neutral}<br/>
+      bad {bad}<br/>
+    </div>
+  )
+}
+
+export default App
